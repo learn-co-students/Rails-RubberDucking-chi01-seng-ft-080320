410dcada459b8bfc4c1090c737f74068c1b4d530
Natalia <n8galan@gmail.com>
2020-08-31 12:59:37 -0500

    finished students deliverables

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,53 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-end
+  def show
+    find_student
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+
+    if @student.valid?
+      flash[:success] = "Welcome #{@student.name}!"
+      redirect_to student_path(@student)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+    
+  end #end of create action
+
+  def edit
+    find_student
+  end
+
+  def update 
+  # because .update when run on an instance does return true or false
+  # no need for .valid?
+    find_student
+    if @student.update(student_params)
+      flash[:success] = "Profile successfully updated!"
+      redirect_to student_path(@student)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path
+    end
+
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
+end #end of StudentsController class
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
+Duck.destroy_all
+Student.destroy_all
+
+puts "seeding"
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
 5.times do
-  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
+  Duck.create(name: Faker::Creature::Dog.meme_phrase, 
+  description: Faker::Creature::Dog.breed,
+  student_id: Student.ids.sample)
 end
+
+puts "done"
+
+# create_table "ducks", force: :cascade do |t|
+#   t.string "name"
+#   t.text "description"
+#   t.integer "student_id"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
+
+# create_table "students", force: :cascade do |t|
+#   t.string "name"
+#   t.integer "mod"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
